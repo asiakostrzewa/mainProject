1fe54cda83a352de6a5ba00e4aeb4b59827f1b9d
asiakostrzewa <joanna.o.kostrzewa@tlen.pl>
2020-10-17 15:20:49 +0200

    .iml deleted

--- demo/src/main/resources/frontend/angular-app/src/app/offer.ts
+++ demo/src/main/resources/frontend/angular-app/src/app/offer.ts
@@ -1,5 +1,23 @@
 export interface Offer {
   id: number;
-continent: string;
-country: string;
+  name: string;
+  continent: string;
+  country: string;
+  city: string;
+  hotel: string;
+  hotelStars: string;
+  hotelDescription: string;
+  airport: string;
+  trip: string;
+  startLocation: string;
+  placeOfDestination: string;
+  leaveDate: string;
+  returnDate: string;
+  howManyDays: string;
+  typeOfFoodEnum: string;
+  priceForAdult: string;
+  priceForChild: string;
+  promoted: string;
+  placesForAdult: string;
+  placesForChild: string;
 }
